209947d174736dea818d2000388341c35ad94caa
Ogeto <rayogeto40@gmail.com>
2024-09-25 00:08:23 +0300

    host

--- aichat/aichat/settings.py
+++ aichat/aichat/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-lzf!rx9m7m&o*%!x-)5&+#sa1u4#rr60g^e3!ou#_fxeb5sr=w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['malariachat.onrender.com']
 
 
 # Application definition
@@ -127,7 +127,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
